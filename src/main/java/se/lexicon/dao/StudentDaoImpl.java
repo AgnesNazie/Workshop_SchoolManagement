3b294f57a8b0fbdafce38e93aac531b6b8400cd3
AgnesNazie <cheagnesmbo@gmail.com>
2025-04-10 11:51:20 +0200

    implemented StudentDoa methods

---
+++ src/main/java/se/lexicon/dao/StudentDaoImpl.java
@@ -0,0 +1,75 @@
+package se.lexicon.dao;
+
+import se.lexicon.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class StudentDaoImpl implements StudentDao {
+    private List<Student> students = new ArrayList<>();
+
+    //save method
+    @Override
+    public Student save(Student student) {
+        // for loop to check if the student is already registered
+        for (Student s : students) {
+            if (s.getId() == student.getId()) {
+                return student;
+            }
+        }
+        // add or save student to the list
+        students.add(student);
+        return student;
+    }
+
+    //find student by email method
+    @Override
+    public Student findByEmail(String email) {
+        //loop through and find a match by email
+        for (Student s : students) {
+            if (s.getEmail().equalsIgnoreCase(email)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    // find student by name method
+    @Override
+    public List<Student> findByName(String name) {
+        List<Student> result = new ArrayList<>();
+        //loop to add matching students
+        for (Student s : students) {
+            if (s.getName().equalsIgnoreCase(name)) {
+                result.add(s);
+            }
+        }
+
+        return result;
+    }
+
+    //find student by id method
+    @Override
+    public Student findById(int id) {
+        //loop through the list to find student by id
+        for (Student s : students) {
+            if (s.getId() == id) {
+                return s;
+            }
+        }
+        return null;
+    }
+    //get all students method
+
+    @Override
+    public List<Student> findAll() {
+        return students;
+    }
+    //delete a student
+
+    @Override
+    public boolean delete(Student student) {
+        return students.remove(student);
+    }
+}
